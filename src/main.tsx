a9c11ded844d97296aa9996fc057dda4b56ded5b
Tristan <75492176+Tirtstan@users.noreply.github.com>
2026-05-18 15:16:32 +0200

    layout fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if (!window.location.hash) {
+  history.scrollRestoration = 'manual'
+  window.scrollTo(0, 0)
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
